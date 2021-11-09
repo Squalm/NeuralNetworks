7ab1d14395181fe67e9da95e0dd9ac925e9ba1db
Squalm <43246103+Squalm@users.noreply.github.com>
2021-11-09 22:18:00 +0000

    quick efficiency boost

--- case_names/gradient_descent.jl
+++ case_names/gradient_descent.jl
@@ -2,10 +2,10 @@
 Update the parameters of the model using the gardients (∇) and the learning rate (η).
 """
 function update_model_weights(parameters, ∇, η)
-    L = floor(length(parameters) / 2)
+    L = trunc(Int, length(parameters) / 2)
 
     # update the parameters (weights and biases) for all the layers
-    for l = 0:trunc(Int, L)-1
+    for l = 0:L-1
         parameters[string("W_", (l+1))] -= η .* ∇[string("δW_", (l+1))]
         parameters[string("b_", (l+1))] -= η .* ∇[string("δb_", (l+1))]
     end # for
